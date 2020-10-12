f0f2c06554b8c7a656f2d48e46f46db60e8bbaf6
Sujan804 <sujanmec804@gmail.com>
2020-10-12 23:36:26 +0600

    Create Modular Exponentiation.cpp
    Complexity = log(n)

---
+++ Number Theory/Modular Exponentiation.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+int modularExponentiation( int x, int n, int M){
+    int result =1;
+    while(n>0){
+        if(n&1){// bitwise check is n is odd
+            result = (result * x)%M;
+        }
+        x = (x*x)%M;
+        n >>= 2;//Bitwise right shift to make half of n
+    }
+    return result;
+
+}
+int main(){
+    int x,n,M;
+    cin>>x>>n>>M;
+    int result = modularExponentiation(x,n,M);
+    cout<<result<<endl;
+
+}
